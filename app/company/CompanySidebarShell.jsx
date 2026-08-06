8ae0088a8aec6bf295f02605c55a5dadd7f0536d
NakulSriram <s.n.sriram2003@gmail.com>
2026-08-06 16:42:20 +0530

    Revert "Company portal UI Responsiveness fixed for all screens"
    This reverts commit 6ec7bf68d7d13308ec2332141b1ade31b940997d.

--- app/company/CompanySidebarShell.jsx
+++ app/company/CompanySidebarShell.jsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import SideBar from "@/modules/company/components/sideBar";
-import CompanyMobileSidebar from "./mobileView/CompanyMobileSidebar";
 import PageStyles from "@/app/student/page.module.scss";
 import useResponsive from "@/hooks/useResponsive";
 import { Suspense } from "react";
@@ -11,7 +10,8 @@ export default function CompanySidebarShell({ children }) {
 
   return (
     <div className={PageStyles.pageContainer} style={{ flexDirection: isMobile ? "column" : "row" }}>
-      {isMobile ? <CompanyMobileSidebar /> : <SideBar isMobile={false} />}
+      {/* TODO: Add MobileSidebar later if needed, use SideBar for now */}
+      <SideBar isMobile={isMobile} />
       <div className={PageStyles.rightColumn} style={{ height: isMobile ? "calc(100vh - 64px)" : "100%" }}>
         <div className={PageStyles.content} style={{ padding: 0, backgroundColor: "#eef5fb" }}>
           <Suspense fallback={null}>
@@ -22,4 +22,3 @@ export default function CompanySidebarShell({ children }) {
     </div>
   );
 }
-
